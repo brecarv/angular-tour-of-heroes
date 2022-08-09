8e9f93a1dbdf88d8e11ba31adaaac40930d18b73
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-08-09 20:15:16 -0300

    mocking and displaying heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,6 +10,7 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
   hero: Hero = { id: 1, name: 'Windstorm' };
+  heroes = HEROES;
 
   constructor() {}
 
