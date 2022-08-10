a75e39e6ede78a64274bf061e0025aa747808ff3
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-08-10 14:32:30 -0300

    observable data

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
@@ -8,7 +9,8 @@ import { HEROES } from './mock-heroes';
 export class HeroService {
   constructor() {}
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
 }
