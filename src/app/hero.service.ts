01333e09bad2ff8429abfe842886fd955cb88f64
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-08-10 21:17:04 -0300

    routable HeroDetailComponent

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,6 +11,12 @@ export class HeroService {
   /* Service-in-Service example*/
   constructor(private messageService: MessageService) {}
 
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find((h) => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
